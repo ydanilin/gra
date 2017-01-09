c67e1e4184740c11f408ad7622b48caf87ad00c6
Yuretzz <yuvede@gmail.com>
2017-01-09 15:48:10 +0300

    del node test added. move subtree needs rework

--- dbms/dbms01.py
+++ dbms/dbms01.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from sqlalchemy import (create_engine, MetaData, Table, Column, Integer,
-                        UniqueConstraint, select)
+                        UniqueConstraint, select, update, and_, or_, column,
+                        literal_column, union)
 
 
 class DBMS:
@@ -27,12 +28,13 @@ class DBMS:
         self.lastLabel = 0
 
     def listDataTable(self):
+        nodes = []
         res = self.connection.execute(select([self.t_data.c.node,
                                               self.t_data.c.parent]
                                              ).order_by(self.t_data.c.node)
                                       )
         nodes = [{'node': l[0], 'parent': l[1]} for l in res]
-        self.lastLabel = nodes[-1]['node']
+        # self.lastLabel = nodes[-1]['node']
         return nodes
 
     def listPathTable(self):
@@ -67,4 +69,40 @@ class DBMS:
         delFromPath = self.t_path.delete().where(self.t_path.c.node == label)
         self.connection.execute(delFromData)
         self.connection.execute(delFromPath)
+        if label == self.lastLabel:
+            self.lastLabel -= 1
         return parent
+
+    def moveSubtree(self, label, moveTo):
+        # update t_data
+        upd = update(self.t_data).where(self.t_data.c.node == label).values(
+            parent=moveTo)
+        self.connection.execute(upd)
+
+        # update t_path
+        subtree = select([self.t_path.c.node]).where(self.t_path.c.ancestor == label)
+        path = select([self.t_path.c.ancestor]).where(self.t_path.c.node == label)
+
+        # deletion
+        cond = and_(or_(self.t_path.c.node == label,
+                        self.t_path.c.node.in_(subtree)),
+                    self.t_path.c.ancestor.in_(path)
+                    )
+        delet = self.t_path.delete().where(cond)
+        self.connection.execute(delet)
+
+        # insertion
+        subtree = select([self.t_path.c.node]).where(self.t_path.c.ancestor == label)
+        path = select([self.t_path.c.ancestor]).where(self.t_path.c.node == label)
+        path_y = select([self.t_path.c.ancestor]).where(self.t_path.c.node == moveTo)
+        subset = select([column('node'), column('ancestor')]).select_from(
+            union(select([literal_column('1').label('sortir'),
+                          literal_column(str(label)).label('node'),
+                          literal_column(str(moveTo)).label('ancestor')]),
+                  select([2, label, path_y]),
+                  select([3, subtree, moveTo]),
+                  select([4, subtree, path_y])).order_by('sortir')
+            )
+        addToPath = self.t_path.insert().from_select(
+            ['node', 'ancestor'], subset)
+        self.connection.execute(addToPath)
--- tests/dbms/test_dbms.py
+++ tests/dbms/test_dbms.py
@@ -3,51 +3,206 @@ import unittest
 from sqlalchemy import (create_engine, MetaData, Table, Column, Integer,
                         UniqueConstraint, select)
 from dbms.dbms01 import DBMS
+from pprint import pprint
 
 
-class DBMS_Test(unittest.TestCase):
-
+class DBMS_Test01(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dbms = DBMS()
-        # cls.data_ins = cls.dbms.t_data.insert(prefixes=['OR IGNORE'])
-        # cls.data_list = [
-        #     {'node': 1, 'parent': 1}
-            # {'node': 2, 'parent': 1},
-            # {'node': 3, 'parent': 2},
-            # {'node': 4, 'parent': 2},
-            # {'node': 5, 'parent': 2},
-            # {'node': 6, 'parent': 1},
-            # {'node': 7, 'parent': 6},
-            # {'node': 8, 'parent': 6}
-        # ]
-        # cls.path_ins = cls.dbms.t_path.insert().prefix_with('OR IGNORE')
-        # cls.path_list = [
-        #     {'node': 2, 'ancestor': 1},
-        #     {'node': 3, 'ancestor': 1},
-        #     {'node': 3, 'ancestor': 2},
-        #     {'node': 4, 'ancestor': 1},
-        #     {'node': 4, 'ancestor': 2},
-        #     {'node': 5, 'ancestor': 1},
-        #     {'node': 5, 'ancestor': 2},
-        #     {'node': 6, 'ancestor': 1},
-        #     {'node': 7, 'ancestor': 1},
-        #     {'node': 7, 'ancestor': 6},
-        #     {'node': 8, 'ancestor': 1},
-        #     {'node': 8, 'ancestor': 6}
-        # ]
-        # cls.dbms.connection.execute(cls.data_ins, cls.data_list)
-        # cls.dbms.connection.execute(cls.path_ins, cls.path_list)
 
-    def test_addChildNode_Root(self):
+    def test_01addChildNode_Root(self):
         tOutput = [{'node': 1, 'parent': 1}]
+        self.assertEqual(self.dbms.lastLabel, 0)
         self.dbms.addChildNode(None)
         res = self.dbms.listDataTable()
         self.assertEqual(self.dbms.lastLabel, 1)
         self.assertEqual(res, tOutput)
 
-    def test_addNode_Many(self):
-        tOutput = [{'node': 1, 'parent': 1}]
+    def test_02addChildNode_Many(self):
+        t_dataOutput = [{'node': 1, 'parent': 1},
+                        {'node': 2, 'parent': 1},
+                        {'node': 3, 'parent': 2},
+                        {'node': 4, 'parent': 2},
+                        {'node': 5, 'parent': 2},
+                        {'node': 6, 'parent': 1},
+                        {'node': 7, 'parent': 6},
+                        {'node': 8, 'parent': 6}]
+        t_pathOutput = [(2, 1),
+                        (3, 1),
+                        (3, 2),
+                        (4, 1),
+                        (4, 2),
+                        (5, 1),
+                        (5, 2),
+                        (6, 1),
+                        (7, 1),
+                        (7, 6),
+                        (8, 1),
+                        (8, 6)]
+        self.assertEqual(self.dbms.lastLabel, 1)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(6)
+        self.dbms.addChildNode(6)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+        self.assertEqual(t_dataRes, t_dataOutput)
+        self.assertEqual(t_pathRes, t_pathOutput)
+        self.assertEqual(self.dbms.lastLabel, 8)
+
+    def test_03addChildNode9(self):
+        t_dataOutput = [{'node': 1, 'parent': 1},
+                        {'node': 2, 'parent': 1},
+                        {'node': 3, 'parent': 2},
+                        {'node': 4, 'parent': 2},
+                        {'node': 5, 'parent': 2},
+                        {'node': 6, 'parent': 1},
+                        {'node': 7, 'parent': 6},
+                        {'node': 8, 'parent': 6},
+                        {'node': 9, 'parent': 2}]
+        t_pathOutput = [(2, 1),
+                        (3, 1),
+                        (3, 2),
+                        (4, 1),
+                        (4, 2),
+                        (5, 1),
+                        (5, 2),
+                        (6, 1),
+                        (7, 1),
+                        (7, 6),
+                        (8, 1),
+                        (8, 6),
+                        (9, 1),
+                        (9, 2)]
+        self.assertEqual(self.dbms.lastLabel, 8)
+        self.dbms.addChildNode(2)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+        self.assertEqual(t_dataRes, t_dataOutput)
+        self.assertEqual(t_pathRes, t_pathOutput)
+        self.assertEqual(self.dbms.lastLabel, 9)
+
+
+class DBMS_Test02(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dbms = DBMS()
+
+    def test_01addNodes(self):
+        self.dbms.addChildNode(None)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(6)
+        self.dbms.addChildNode(6)
+        self.assertEqual(self.dbms.lastLabel, 8)
+
+    def test_02deleteNode4(self):
+        t_dataOutput = [{'node': 1, 'parent': 1},
+                        {'node': 2, 'parent': 1},
+                        {'node': 3, 'parent': 2},
+                        {'node': 5, 'parent': 2},
+                        {'node': 6, 'parent': 1},
+                        {'node': 7, 'parent': 6},
+                        {'node': 8, 'parent': 6}]
+        t_pathOutput = [(2, 1),
+                        (3, 1),
+                        (3, 2),
+                        (5, 1),
+                        (5, 2),
+                        (6, 1),
+                        (7, 1),
+                        (7, 6),
+                        (8, 1),
+                        (8, 6)]
+        self.dbms.deleteLeafNode(4)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+        self.assertEqual(t_dataRes, t_dataOutput)
+        self.assertEqual(t_pathRes, t_pathOutput)
+        self.assertEqual(self.dbms.lastLabel, 8)
+
+        del t_dataOutput[-1]
+        del t_pathOutput[-1]
+        del t_pathOutput[-1]
+        self.dbms.deleteLeafNode(8)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+        self.assertEqual(t_dataRes, t_dataOutput)
+        self.assertEqual(t_pathRes, t_pathOutput)
+        self.assertEqual(self.dbms.lastLabel, 7)
+
+
+class DBMS_Test03(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.dbms = DBMS()
+
+    def test_01addNodes(self):
+        self.dbms.addChildNode(None)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(2)
+        self.dbms.addChildNode(1)
+        self.dbms.addChildNode(6)
+        self.dbms.addChildNode(6)
+        self.dbms.addChildNode(6)
+        self.assertEqual(self.dbms.lastLabel, 9)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+        # pprint(t_dataRes)
+        # pprint(t_pathRes)
+
+    def test_02moveSubtree_2_7(self):
+        t_dataOutput = [{'node': 1, 'parent': 1},
+                        {'node': 6, 'parent': 1},
+                        {'node': 7, 'parent': 6},
+                        {'node': 2, 'parent': 7},
+                        {'node': 3, 'parent': 2},
+                        {'node': 4, 'parent': 2},
+                        {'node': 5, 'parent': 2},
+                        {'node': 8, 'parent': 6},
+                        {'node': 9, 'parent': 6}]
+        t_pathOutput = [(6, 1),
+                        (7, 6),
+                        (7, 1),
+                        (2, 7),
+                        (2, 6),
+                        (2, 1),
+                        (3, 2),
+                        (3, 7),
+                        (3, 6),
+                        (3, 1),
+                        (4, 2),
+                        (4, 7),
+                        (4, 6),
+                        (4, 1),
+                        (5, 2),
+                        (5, 7),
+                        (5, 6),
+                        (5, 1),
+                        (8, 6),
+                        (8, 1),
+                        (9, 6),
+                        (9, 1)
+                        ]
+        self.dbms.moveSubtree(2, 7)
+        t_dataRes = self.dbms.listDataTable()
+        t_pathRes = self.dbms.listPathTable()
+
+        pprint(t_dataRes)
+        pprint(t_pathRes)
+        self.assertEqual(t_dataRes, t_dataOutput)
+        self.assertEqual(t_pathRes, t_pathOutput)
+        self.assertEqual(self.dbms.lastLabel, 9)
+
 
 if __name__ == '__main__':
     unittest.main()
