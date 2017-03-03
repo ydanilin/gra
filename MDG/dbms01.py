30bf24a23ce41d13946ef8efbb43af9ad6cd30aa
Yuretzz <yuvede@gmail.com>
2017-03-03 16:18:27 +0300

    struggle against whitespace

--- MDG/dbms01.py
+++ MDG/dbms01.py
@@ -2,7 +2,10 @@
 """DjHuj is the best DJ all over the world"""
 
 class FrontEnd:
-    def __init__(self,puk:int):
+    def __init__(self, puk : int):
         self.puk = 1
-        # bduk = 2
+        bduk = 2
+        self.her = 77
+    def bduk( self  , djhuj:str):
+        huj = 15
 huj = 4
