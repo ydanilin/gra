080f79de0d9d1b11a7e9fc43fa7edbf4a9eff4e7
Yuretzz <yuvede@gmail.com>
2017-04-02 21:44:08 +0300

    analyze calls

--- models/treemodel.py
+++ models/treemodel.py
@@ -17,13 +17,33 @@ class TreeModel(QAbstractItemModel):
                               5: ['geometry', 'centerX'],
                               6: ['geometry', 'centerY']}
         self.columnsAmt = len(self.columns)
-        # rootData = []
-        # rootData.append('Label')
-        # rootData.append('Summary')
-        # self.rootItem = TreeItem(rootData)
         self.setupModelData(datta)
+        # model, proc, row, col, assData, role
+        self.msg = 'S\t{0}\t{1}\t{2}\t{3}\t{4}'
+        self.roles = {0: 'DisplayRole',
+                      1: 'DecorationRole',
+                      2: 'EditRole',
+                      3: 'ToolTipRole',
+                      4: 'StatusTipRole',
+                      5: 'WhatsThisRole',
+                      6: 'FontRole',
+                      7: 'TextAlignmentRole',
+                      8: 'BackgroundRole',
+                      9: 'ForegroundRole',
+                      10: 'CheckStateRole',
+                      11: 'AccessibleTextRole',
+                      12: 'AccessibleDescriptionRole',
+                      13: 'SizeHintRole',
+                      14: 'InitialSortOrderRole'
+                      }
 
     def index(self, row, column, parent=None, *args, **kwargs):
+        # DEBUG
+        assData = None
+        if parent.isValid():
+            assData = parent.internalPointer().datta(['node'])
+        print(self.msg.format('index()', row, column, assData, 'N/A'))
+        # DEBUG ends
         if not self.hasIndex(row, column, parent):
             return QModelIndex()
         parentItem: object = None
@@ -38,6 +58,14 @@ class TreeModel(QAbstractItemModel):
             return QModelIndex()
 
     def parent(self, index):
+        # DEBUG
+        row = index.row()
+        column = index.column()
+        assData = None
+        if index.isValid():
+            assData = index.internalPointer().datta(['node'])
+        print(self.msg.format('parent()', row, column, assData, 'N/A'))
+        # DEBUG ends
         if not index.isValid():
             return QModelIndex()
         childItem = index.internalPointer()
@@ -47,8 +75,16 @@ class TreeModel(QAbstractItemModel):
         return self.createIndex(parentItem.row(), 0, parentItem)
 
     def rowCount(self, parent=None, *args, **kwargs):
-        # if parent.column() > 0:
-        #     return 0
+        # DEBUG
+        row = parent.row()
+        column = parent.column()
+        assData = None
+        if parent.isValid():
+            assData = parent.internalPointer().datta(['node'])
+        print(self.msg.format('rowCount()', row, column, assData, 'N/A'))
+        # DEBUG ends
+        if parent.column() > 0:
+            return 0
         if not parent.isValid():
             parentItem = self.rootItem
         else:
@@ -56,6 +92,14 @@ class TreeModel(QAbstractItemModel):
         return parentItem.childCount()
 
     def columnCount(self, parent=None, *args, **kwargs):
+        # DEBUG
+        row = parent.row()
+        column = parent.column()
+        assData = None
+        if parent.isValid():
+            assData = parent.internalPointer().datta(['node'])
+        print(self.msg.format('columnCount()', row, column, assData, 'N/A'))
+        # DEBUG ends
         # if parent.isValid():
         #     return parent.internalPointer().columnCount()
         # else:
@@ -63,19 +107,47 @@ class TreeModel(QAbstractItemModel):
         return self.columnsAmt
 
     def data(self, index, role=None):
+        # DEBUG
+        row = index.row()
+        column = index.column()
+        assData = None
+        if index.isValid():
+            assData = index.internalPointer().datta(['node'])
+        roll = 'Not Listed'
+        if role in self.roles.keys():
+            roll = self.roles[role]
+        print(self.msg.format('data()', row, column, assData, roll))
+        # DEBUG ends
         if not index.isValid():
             return None
         if role != Qt.DisplayRole:
             return None
         item = index.internalPointer()
-        return item.datta(self.columns[index.column()])
+        return str(item.datta(self.columns[index.column()]))
 
     def flags(self, index):
+        # DEBUG
+        row = index.row()
+        column = index.column()
+        assData = None
+        if index.isValid():
+            assData = index.internalPointer().datta(['node'])
+        print(self.msg.format('flags()', row, column, assData, 'N/A'))
+        # DEBUG ends
         if not index.isValid():
             return Qt.NoItemFlags
         return super(TreeModel, self).flags(index)
 
     def headerData(self, section, orientation, role=None):
+        # DEBUG
+        row = 'ori:' + str(orientation)
+        column = section
+        assData = None
+        roll = 'Not Listed'
+        if role in self.roles.keys():
+            roll = self.roles[role]
+        print(self.msg.format('headerData()', row, column, assData, roll))
+        # DEBUG ends
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self.columns[section][-1]
         return None
@@ -124,15 +196,15 @@ class TreeViewModel(QIdentityProxyModel):
 
     def headerData(self, section, orientation, role=None):
         if orientation == Qt.Horizontal and (role == Qt.DisplayRole or
-                                             role == Qt.SizeHintRole):
+                                                     role == Qt.SizeHintRole):
             modelHeader = self.sourceModel().columns[section][-1]
             mappedName = modelHeader
             if modelHeader in self.nameMapping.keys():
                 mappedName = self.nameMapping[modelHeader]
             if role == Qt.DisplayRole:
                 return mappedName
-            # if role == Qt.SizeHintRole:
-            #     return QSize(300, 30)
+                # if role == Qt.SizeHintRole:
+                #     return QSize(300, 30)
         return None
 
     def data(self, index, role=None):
@@ -158,7 +230,8 @@ class TreeViewModel(QIdentityProxyModel):
         output = super(TreeViewModel, self).flags(index)
         if index.column() == 3:
             output = output | Qt.ItemIsEditable  # "|" = bitwise OR Python
-        return output                            # "|" = bitwise OR C++
+        return output  # "|" = bitwise OR C++
+
 
 # http://stackoverflow.com/questions/32822442/how-to-align-text-of-table-widget-in-qt
 
@@ -167,19 +240,34 @@ class ExpModel(QAbstractProxyModel):
     def __init__(self, sourceModel, parent=None):
         super(ExpModel, self).__init__(parent)
         self.setSourceModel(sourceModel)
-        self.sourceIndexes: list = [0, 4, 6]
+        self.sourceIndexes: list = [6, 0, 5]
         self.columnsAmt: int = len(self.sourceIndexes)
 
-    def mapFromSource(self, index):
-        col = index.column()
-        if col in self.sourceIndexes:
-            return self.createIndex(index.row(), self.sourceIndexes.index(col), index.internalPointer())
+    def mapFromSource(self, sourceIndex):
+        sCol = sourceIndex.column()
+        print(sCol)
+        if sCol in self.sourceIndexes:
+            return self.createIndex(sourceIndex.row(),
+                                    self.sourceIndexes.index(sCol),
+                                    sourceIndex.internalPointer())
+        # if sCol == -1:
+        #     return self.createIndex(sourceIndex.row(), sCol,
+        #                             sourceIndex.internalPointer())
         else:
-            return self.createIndex(index.row(), col, index.internalPointer())
+            return self.createIndex(sourceIndex.row(), sCol,
+                                    sourceIndex.internalPointer())
 
     def mapToSource(self, index):
-        col = self.sourceIndexes[index.column()]
-        return self.sourceModel().createIndex(index.row(), col, index.internalPointer())
+        requestedColumn = index.column()
+        # print(requestedColumn)
+        if requestedColumn == -1:
+            return self.sourceModel().createIndex(index.row(),
+                                                  requestedColumn,
+                                                  index.internalPointer())
+        mappedColumn = self.sourceIndexes[requestedColumn]
+        return self.sourceModel().createIndex(index.row(),
+                                              mappedColumn,
+                                              index.internalPointer())
 
     def rowCount(self, parent=None, *args, **kwargs):
         return self.sourceModel().rowCount(parent)
@@ -202,9 +290,9 @@ class ExpModel(QAbstractProxyModel):
         sourceParent = sourceIndex.parent()
         return self.mapFromSource(sourceParent)
 
-    # def flags(self, index):
-    #     if not index.isValid():
-    #         return Qt.NoItemFlags
-    #     return super(ExpModel, self).flags(index)
+        # def flags(self, index):
+        #     if not index.isValid():
+        #         return Qt.NoItemFlags
+        #     return super(ExpModel, self).flags(index)
 
 # http://stackoverflow.com/questions/23896182/sourcemodel-createindex-in-qabstractproxymodel-sub-class?rq=1
